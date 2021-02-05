5633446d490f644ebc238d6d72a0f64de39c81b9
houssem boukari <houssemliver1@outlook.com>
2021-02-05 23:27:15 +0100

    dml

---
+++ check.sql
@@ -0,0 +1,52 @@
+INSERT INTO Product VALUES
+(
+"P01",
+"Sumsung Galaxy S20",
+"Smartphone",
+3299
+);
+INSERT INTO Product VALUES
+(
+"P02",
+"ASUS Notebook",
+"Pc",
+4599
+);
+/**************/
+INSERT INTO Customer VALUES
+(
+"C01",
+"ALI",
+71321009
+);
+INSERT INTO Customer VALUES
+(
+"C02",
+"ASMA",
+77345823
+);
+/**************/
+INSERT INTO Orders VALUES
+(
+OrderDate,
+Quantity,
+Total_amount
+)
+VALUES
+(
+NULL,
+2,
+9198
+);
+INSERT INTO Orders VALUES
+(
+OrderDate,
+Quantity,
+Total_amount
+)
+VALUES
+(
+28/05/2020,
+1,
+3299
+);
